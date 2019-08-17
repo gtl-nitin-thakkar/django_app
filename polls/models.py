d3721b940716a0a988d6dc2f088d80d4a16ba51b
Nitin Thakkar <nitin.thakkar@thegatewaycorp.com>
2019-08-17 23:27:10 +0530

    API- Question detail with choices

--- polls/models.py
+++ polls/models.py
@@ -8,16 +8,20 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-	
-	def verbose_question_text(self):
+
+    def verbose_question_text(self):
         return "Question : %s" % (self.question_text)
-		
+
+    def choices(self):
+        if not hasattr(self, '_choices'):
+            self._choices = self.choice_set.all()
+        return self._choices
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -26,3 +30,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
--- polls/serializers.py
+++ polls/serializers.py
@@ -2,11 +2,18 @@ from rest_framework import serializers
 from .models import Question
 from .models import Choice
 
+class ChoiceSerializer(serializers.Serializer):
+    choice_text = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return Choice.objects.create(**validated_data)
+    
 class QuestionSerializer(serializers.Serializer):
     question_text = serializers.CharField(max_length=200)
     pub_date = serializers.DateTimeField()
     was_published_recently = serializers.BooleanField(read_only=True)
     verbose_question_text = serializers.CharField(read_only=True)
+    choices = ChoiceSerializer(many=True, read_only=True)
 
     # DRF serializer.save() calls self.create(self.validated_data)
     def create(self, validated_data):
@@ -18,11 +25,3 @@ class QuestionSerializer(serializers.Serializer):
             setattr(instance, key, value)
         instance.save()
         return instance
-
-
-class ChoiceSerializer(serializers.Serializer):
-    choice_text = serializers.CharField(max_length=200)
-
-    def create(self, validated_data):
-        return Choice.objects.create(**validated_data)
-
